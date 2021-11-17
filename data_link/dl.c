1b7452764c40c73bc2ec40e312c194b42215af11
Catarina Pires <catarina.o.pires@hotmail.com>
2021-11-17 18:17:49 +0000

    Completed llclose

--- data_link/dl.c
+++ data_link/dl.c
@@ -65,27 +65,26 @@ int llopen(char *serial_port, user_type_t type) {
   }
 
   uint8_t msg_byte = 0;
+  uint8_t packet[CTRL_PACKET_SIZE];
 
   switch (type) {
       
     case EMITTER: {
         int tries = 0;
-        uint8_t SET_packet[CTRL_PACKET_SIZE];
-        uint8_t UA_packet[CTRL_PACKET_SIZE];
-
-        create_control_packet(FRAME_CTRL_SET, FRAME_ADDR_EM, SET_packet);
-
-        (void) signal(SIGALRM, signal_handler); 
+        create_control_packet(FRAME_CTRL_SET, FRAME_ADDR_EM, packet);
+        signal(SIGALRM, signal_handler); 
 
         do {
           tries++;
           printf("nr try: %d\n", tries);
-          int n = write(port_fd, SET_packet, CTRL_PACKET_SIZE);
+          // WRITE SET
+          int n = write(port_fd, packet, CTRL_PACKET_SIZE);
 
           alarm(3);
           failed_to_read = true;
           state = START;
 
+          // READ UA
           while (state != STOP && failed_to_read) {
             read(port_fd, &msg_byte, 1);
             printf("e: read %x\n", msg_byte);
@@ -103,20 +102,20 @@ int llopen(char *serial_port, user_type_t type) {
     break;
 
     case RECEIVER: {
+      // READ SET
       while (state != STOP) {
         read(port_fd, &msg_byte, 1);
         printf("r: read %x\n", msg_byte);
         check_control_packet_byte(msg_byte, FRAME_CTRL_SET, FRAME_ADDR_EM, &state);
       }
 
+      // WRITE UA
       if (state == STOP) {
 
-        uint8_t UA_packet[CTRL_PACKET_SIZE];
-        create_control_packet(FRAME_CTRL_UA, FRAME_ADDR_REC, UA_packet);
-        
-        int n = write(port_fd, UA_packet, CTRL_PACKET_SIZE);
+        create_control_packet(FRAME_CTRL_UA, FRAME_ADDR_REC, packet);
+        int n = write(port_fd, packet, CTRL_PACKET_SIZE);
 
-        printf("r(SENT) ua: write %x %x %x %x %x\n", UA_packet[0], UA_packet[1], UA_packet[2], UA_packet[3] , UA_packet[4]);
+        printf("r(SENT) ua: write %x %x %x %x %x\n", packet[0], packet[1], packet[2], packet[3] , packet[4]);
 
         if (n < 0) {
           perror(serial_port);
@@ -214,12 +213,106 @@ int llread(int fd, char *buffer) {
 
 
 int llclose(int port_fd, user_type_t type) {
-  sleep(2);
+  uint8_t msg_byte = 0;
+  uint8_t packet[CTRL_PACKET_SIZE];
+  failed_to_read = true;
 
+  switch (type) {
+      
+    case EMITTER: {
+        int tries = 0;
+        create_control_packet(FRAME_CTRL_DISC, FRAME_ADDR_EM, packet);
+        signal(SIGALRM, signal_handler); 
+
+        do {
+          tries++;
+          printf("nr try: %d\n", tries);
+
+          // WRITE DISC
+          int n = write(port_fd, packet, CTRL_PACKET_SIZE);
+          printf("Termination of connection\n");
+
+          alarm(3);
+          failed_to_read = true;
+          state = START;
+
+          // READ DISC
+          while (state != STOP && failed_to_read) {
+            read(port_fd, &msg_byte, 1);
+            printf("e: read %x\n", msg_byte);
+            check_control_packet_byte(msg_byte, FRAME_CTRL_DISC, FRAME_ADDR_REC, &state);
+          }
+
+          if (state == STOP) {
+            failed_to_read = false;
+            printf("UA received on time\n");
+          }
+
+        } while (tries < MAX_NR_TRIES && failed_to_read);
+
+        if (state != STOP) {
+          printf("Failed: timeout");
+          return -1;
+        }
+
+        // WRITE UA
+        create_control_packet(FRAME_CTRL_UA, FRAME_ADDR_EM, packet);
+        write(port_fd, packet, CTRL_PACKET_SIZE);
+        printf("Disconnected\n");
+    }
+
+    break;
+
+    case RECEIVER: {
+      // READ DISC
+      while (state != STOP) {
+        read(port_fd, &msg_byte, 1);
+        printf("r: read %x\n", msg_byte);
+        check_control_packet_byte(msg_byte, FRAME_CTRL_DISC, FRAME_ADDR_EM, &state);
+      }
+
+      if (state != STOP) {
+        printf("Failed: no DISC received\n");
+        return -1;
+      }
+
+      // WRITE DISC
+      create_control_packet(FRAME_CTRL_DISC, FRAME_ADDR_REC, packet);
+      int n = write(port_fd, packet, CTRL_PACKET_SIZE);
+
+      if (n < 0) {
+        perror("fd");
+        return -1;
+      }
+
+      // READ UA
+      state = START;
+
+      while (state != STOP) {
+        read(port_fd, &msg_byte, 1);
+        printf("r: read %x\n", msg_byte);
+        check_control_packet_byte(msg_byte, FRAME_CTRL_UA, FRAME_ADDR_EM, &state);
+      }
+
+      if (state != STOP) {
+        printf("Failed: no UA received\n");
+        return -1;
+      }
+
+      printf("Disconnected\n");
+      
+    } 
+    break;
+  }
+
+
+  sleep(2);
   if (tcsetattr(port_fd, TCSANOW, &oldtio) < 0) {
     perror("tcsetattr");
     return -1;
   }
+
   close(port_fd);
-  return 0;
+
+  return 1;
 }
